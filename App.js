ba4c57283fc1e3f71fbd239b1216c67f13281234
Amelia Michelle Jonaidy <23002502@myrp.edu.sg>
2024-10-23 14:35:21 +0800

    commit updated styles

--- App.js
+++ App.js
@@ -3,49 +3,54 @@ import React from 'react';
 import Boat from "./components/Boats";
 import Icon from "react-native-vector-icons/FontAwesome6";
 
-
 // App component
 const App = () => {
-  return (
-      <ScrollView>
-        <Boat
-            name="Sea Ray 500 Sundancer"
-            description="Contoured lines and dramatic styling reveal a refined and powerful presence that will take your breath away."
-            poster={require("./img/sea_ray.jpg")}
-            icon_name="sailboat"
-        />
-          <Boat
-              name="Four Winns Horizon 180"
-              description="A sporty look and refined details truly set the Horizon 180 above all others."
-              poster={require("./img/four_winns.jpg")}
-              icon_name="sailboat"
-          />
-          <Boat
-              name="Flipper 640 ST"
-              description="A modern take on the classic, traditional hardtop and perfect for a family picnic."
-              poster={require("./img/flipper.jpg")}
-              icon_name="sailboat"
-          />
-          <Boat
-              name="Princess V48"
-              description="A modern take on the classic, traditional hardtop and perfect for a family picnic.There is the option for an open design with a full-length cockpit and sunroof, or the enclosed deck saloon model, available with the option of a climate controlled interior."
-              poster={require("./img/princess.jpg")}
-              icon_name="sailboat"
-          />
-          <Boat
-              name="Bayliner 175 Bowrider"
-              description="Its outboard power gives you increased cockpit space and quiet, fuel-efficient performance."
-              poster={require("./img/bayliner.jpg")}
-              icon_name="sailboat"
-          />
-          <Boat
-              name="Fairline Targa 47"
-              description="Stretch out on the large sun bed aft while friends lounge in the generously appointed cockpit."
-              poster={require("./img/fairline.jpg")}
-              icon_name="sailboat"
-          />
-      </ScrollView>
-  );
+    return (
+        <View>
+            <Text></Text>
+            <Text style={{ fontSize: 24, fontWeight: 'bold', textAlign: 'center', margin: 15 }}>
+                GetABoat - For Sale
+            </Text>
+            <ScrollView>
+                <Boat
+                    name="Sea Ray 500 Sundancer"
+                    description="Contoured lines and dramatic styling reveal a refined and powerful presence that will take your breath away."
+                    poster={require("./img/sea_ray.jpg")}
+                    icon_name="sailboat"
+                />
+                <Boat
+                    name="Four Winns Horizon 180"
+                    description="A sporty look and refined details truly set the Horizon 180 above all others."
+                    poster={require("./img/four_winns.jpg")}
+                    icon_name="sailboat"
+                />
+                <Boat
+                    name="Flipper 640 ST"
+                    description="A modern take on the classic, traditional hardtop and perfect for a family picnic."
+                    poster={require("./img/flipper.jpg")}
+                    icon_name="sailboat"
+                />
+                <Boat
+                    name="Princess V48"
+                    description="A modern take on the classic, traditional hardtop and perfect for a family picnic. There is the option for an open design with a full-length cockpit and sunroof, or the enclosed deck saloon model, available with the option of a climate-controlled interior."
+                    poster={require("./img/princess.jpg")}
+                    icon_name="sailboat"
+                />
+                <Boat
+                    name="Bayliner 175 Bowrider"
+                    description="Its outboard power gives you increased cockpit space and quiet, fuel-efficient performance."
+                    poster={require("./img/bayliner.jpg")}
+                    icon_name="sailboat"
+                />
+                <Boat
+                    name="Fairline Targa 47"
+                    description="Stretch out on the large sun bed aft while friends lounge in the generously appointed cockpit."
+                    poster={require("./img/fairline.jpg")}
+                    icon_name="sailboat"
+                />
+            </ScrollView>
+        </View>
+    );
 };
 
 export default App;
--- components/Boats.js
+++ components/Boats.js
@@ -1,13 +1,18 @@
-import {Image, Text, View} from "react-native";
+import { Image, Text, View } from "react-native";
 import React from "react";
 import Icon from "react-native-vector-icons/FontAwesome6";
 
 const Boat = ({ name, description, icon_name, poster }) => {
     return (
         <View>
-            <Icon name={icon_name} size={20}><Text>{name}</Text></Icon>
-            <Text>{description}</Text>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <Icon name={icon_name} size={30} />
+                <Text style={{ fontSize: 24, color: 'blue', marginLeft: 8 }}>{name}</Text>
+            </View>
+            <Text style={{ fontSize: 15,color: 'red' }}>{description}</Text>
             <Image source={poster} style={{ width: 400, height: 500 }} />
+            <Text></Text>
+            <Text></Text>
         </View>
     );
 };
